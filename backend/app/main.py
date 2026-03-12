4549ee33b08e325b5b9fb872e67062fb96b35bfa
Y8724 <yanaysang@gmail.com>
2026-03-12 22:27:37 +0100

    Fix CORS syntax error

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
      "https://your-frontend.netlify.app"
-    ]
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
