0b212274e2832a7b95ab2badb7acd8472a244dfa
Y8724 <yanaysang@gmail.com>
2026-03-26 12:48:28 +0100

    grid fixed

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -180,7 +180,7 @@ export default function Books() {
       </div>
 
       {/* GRID */}
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start">
 
         {filteredBooks.map((book) => (
           <div
